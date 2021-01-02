1bba2ca6e2be2bae9f7ee8505009c3d1504e57c8
Groot <neshamlani1970@gmail.com>
2021-01-02 10:03:07 +0530

    Starting point for Version 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
-import Header from './Header';
-import Banner from './Banner';
-import Details from './Details';
 import './App.css';
 
 const App = () => {
 
   return (
     <div className='root'>
-      <Banner />
-      <Header />
-      <MediaQuery maxWidth={600}>
-        <Details isMobile />
-      </MediaQuery>
-      <MediaQuery minWidth={600}>
-        <Details />
-      </MediaQuery>
+      
     </div>
   )
 }
--- src/Spinner/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './index.css';
-const Spinner = () => (
-  <div className='loader'></div>
-)
-
-export default Spinner;
